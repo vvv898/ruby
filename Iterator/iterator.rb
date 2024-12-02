dadc4fe6684540179fd9f29000b60b0d4df5d941
Viktoria Pekar <viktoriia.pekar@student.karazin.ua>
2024-12-02 20:56:23 +0200

    new

--- Iterator/iterator.rb
+++ Iterator/iterator.rb
@@ -1,12 +1,19 @@
 class AlphabeticalOrderIterator
+  # У Ruby модуль Enumerable надає класам декілька методів для обходу і пошуку,
+  # а також можливість сортування. Клас повинен забезпечувати метод `each`,
+  # який передає наступні елементи колекції.
   include Enumerable
 
+  # Цей атрибут вказує напрямок обходу.
   attr_accessor :reverse
   private :reverse
 
+  # @return [Array]
   attr_accessor :collection
   private :collection
 
+  # @param [Array] collection
+  # @param [Boolean] reverse
   def initialize(collection, reverse: false)
     @collection = collection
     @reverse = reverse
@@ -20,6 +27,7 @@ class AlphabeticalOrderIterator
 end
 
 class WordsCollection
+  # @return [Array]
   attr_accessor :collection
   private :collection
 
@@ -27,14 +35,18 @@ class WordsCollection
     @collection = collection
   end
 
+  # Метод `iterator` повертає об'єкт ітератора, за замовчуванням ми
+  # повертаємо ітератор у порядку зростання.
   def iterator
     AlphabeticalOrderIterator.new(@collection)
   end
 
+  # @return [AlphabeticalOrderIterator]
   def reverse_iterator
     AlphabeticalOrderIterator.new(@collection, reverse: true)
   end
 
+  # @param [String] item
   def add_item(item)
     @collection << item
   end
@@ -45,9 +57,9 @@ collection.add_item('First')
 collection.add_item('Second')
 collection.add_item('Third')
 
-puts 'Straight traversal:'
+puts 'Прямий обхід:'
 collection.iterator.each { |item| puts item }
 puts "\n"
 
-puts 'Reverse traversal:'
+puts 'Зворотній обхід:'
 collection.reverse_iterator.each { |item| puts item }
